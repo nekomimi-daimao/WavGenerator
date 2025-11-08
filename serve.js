9f25b80008ac87179aeea6e2712eb057590a0275
nekomimi-daimao <35223467+nekomimi-daimao@users.noreply.github.com>
2025-11-08 14:48:11 +0900

    ローカル起動用のdenoスクリプトを追加

---
+++ serve.js
@@ -0,0 +1,6 @@
+// deno --allow-net --allow-read --watch=. serve.js
+// http://localhost:4410/
+
+import { serveDir } from "jsr:@std/http@1";
+
+Deno.serve({ port: 4410 }, request => serveDir(request, { fsRoot: "./static", urlRoot: ""}));
